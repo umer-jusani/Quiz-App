e62add65a697e627bbfbeb1d48184761a9646ef4
Umer <umer.jusani.04@gmail.com>
2024-01-17 22:51:42 +0500

    Initialize Repo

---
+++ src/Quiz.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Quiz = ({ data }) => {
+  const [currentQuestion, setcurrentQuestions] = useState(0);
+
+  return (
+    <div className="quiz-container">
+      <h1>{currdata.currdata}</h1>
+    </div>
+  );
+};
+
+export default Quiz;
