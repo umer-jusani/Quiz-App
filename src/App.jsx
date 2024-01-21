138cafe40690dbf351b3241a53ed04b100717bf4
Umer <umer.jusani.04@gmail.com>
2024-01-21 17:33:47 +0500

    styled question quiz box

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Quiz from "./Quiz";
-import axios from "axios";
 
 function App() {
-  const [data, Setdata] = useState();
-
-  const FetchingData = async () => {
-    axios
-      .get("https://644982a3e7eb3378ca4ba471.mockapi.io/questions")
-      .then((res) => {
-        Setdata(res.data);
-      })
-      .catch((err) => {
-        console.log("Getting Error");
-      });
-  };
-
-  useEffect(() => {
-    FetchingData();
-  }, []);
-
   return (
     <>
-      <Quiz data={data} />
+      <Quiz />
     </>
   );
 }
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 
-const Quiz = ({ data }) => {
-  const [currentQuestion, setcurrentQuestions] = useState(0);
+const Quiz = () => {
+  const [data, setData] = useState();
+  const [currentData, SetcurrentData] = useState(0);
 
-  return (
-    <div className="quiz-container">
-      <h1>{currdata.currdata}</h1>
-    </div>
-  );
+  const fetchingData = async () => {
+    const res = await axios.get(
+      "https://644982a3e7eb3378ca4ba471.mockapi.io/questions"
+    );
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  if (data) {
+    const { question, choices, id } = data[currentData];
+
+    return (
+      <div className="p-12 bg-white font-mono space-y-7 rounded-md">
+        <div>
+          <h1 className="text-gray-500 mb-2">
+            <span className="text-[#2d264b] text-5xl">{id + 1}</span>/
+            {data.length}
+          </h1>
+          <h3 className="font-medium text-xl">{question}</h3>
+        </div>
+        <ul>
+          
+        </ul>
+        <div className="flex justify-end">
+          <button
+            className="btn"
+            onClick={() => SetcurrentData(currentData + 1)}
+          >
+            {currentData == data.length - 1 ? "Finish" : "Next"}
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Quiz;
